995bda7aa6c52635a0fcf8b18f1ed47d91c58b92
Ajaysvasan <ajay192006@gmail.com>
2025-02-26 19:58:03 +0530

    some bugs and some deletion day 2 of me being lazy ass

--- src/speech/sst.py
+++ src/speech/sst.py
@@ -1,14 +1,27 @@
-import whisper
+import faster_whisper
 import os
+import pyaudio 
 
-model = whisper.load_model("medium")
+model = faster_whisper.WhisperModel("tiny",compute_type="float16")
 
 class SpeechToText:
     def __init__(self,model):
         os.environ["PATH"] += os.pathsep + r"C:\ffmpeg\bin"
+        self.chunck = 1024
         self.model = model
+        self.p = pyaudio.PyAudio()
 
+    def recordChunck(p,stream,filePath,chuckLength = 1):
+        frames = []
+        for _ in range(0,int(16000/1024 * chuckLength)):
+            data = stream.read(1024)
+            frames.append(data)
+        return ""
     def recognize_speech(self,audioFile):
+        stream = self.p.open(format=pyaudio.paInt16,channels=1,rate = 16000,input = True,frames_per_buffer=1024)
+        accumulatedTranscription = ""
+
+                
         return self.model.transcribe(audioFile)['text']
 
 
