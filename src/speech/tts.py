4262fbcf25147234b9c3ed6efee15b409cde936a
Ajaysvasan <ajay192006@gmail.com>
2025-03-09 20:45:10 +0530

    IDK wtf is going on

--- src/speech/tts.py
+++ src/speech/tts.py
@@ -29,17 +29,105 @@ class PhonemeMapper:
         self.phoneme_map = self.build_phoneme_map()
 
     def build_phoneme_map(self):
-        pass
+        
+        digraphMap = {
+            "ch": "CH",
+            "sh": "SH",
+            "th": {"voiceless": "TH", "voiced": "DH"},
+            "ph": "F",
+            "ck": "K",
+            "gh": "G",
+            "wh": "W",
+            "kn": "N",
+            "wr": "R",
+            "qu": "KW"
+        }
 
-    def apply_contextual_rules(self, word):
-        pass
+        singlLetteRmap = {
+            "a": "AH",
+            "b": "B",
+            "c": {"default": "K", "soft": "S"},
+            "d": "D",
+            "e": "EH",
+            "f": "F",
+            "g": {"default": "G", "soft": "J"},
+            "h": "HH",
+            "i": "IH",
+            "j": "JH",
+            "k": "K",
+            "l": "L",
+            "m": "M",
+            "n": "N",
+            "o": "OW",
+            "p": "P",
+            "q": "K",
+            "r": "R",
+            "s": "S",
+            "t": "T",
+            "u": "UH",
+            "v": "V",
+            "w": "W",
+            "x": "KS",
+            "y": "Y",
+            "z": "Z"
+        }
 
-    def map_word_to_phonemes(self, word):
+        return {
+            "digraphs": digraphMap,
+            "singles": singlLetteRmap
+        }
+
+
+    def apply_contextual_rules(self, word:str):
         pass
+    
+    def get_th_phoneme(self, word, index):
+        voicedWords = ["this", "that", "the", "those", "these"]
+        if word.lower() in voicedWords:
+            return self.phoneme_map["digraphs"]["th"]["voiced"]
+        return self.phoneme_map["digraphs"]["th"]["voiceless"]
+
+    def map_word_to_phonemes(self, word: str) -> list:
+        phonemes = []
+        i = 0
+        while i < len(word):
+            if i + 1 < len(word):
+                digraph = word[i:i+2]
+                if digraph in self.phoneme_map["digraphs"]:
+                    if digraph == "th":
+                        phoneme = self.get_th_phoneme(word, i)
+                    else:
+                        phoneme = self.phoneme_map["digraphs"][digraph]
+                    phonemes.append(phoneme)
+                    i += 2
+                    continue
+            
+            char = word[i]
+            next_char = word[i+1] if i+1 < len(word) else ""
+
+            if char in self.phoneme_map["singles"]:
+                if char == "c":
+                    if next_char in ['e', 'i', 'y']:
+                        phoneme = self.phoneme_map["singles"]["c"]["soft"]
+                    else:
+                        phoneme = self.phoneme_map["singles"]["c"]["default"]
+                elif char == "g":
+                    if next_char in ['e', 'i', 'y']:
+                        phoneme = self.phoneme_map["singles"]["g"]["soft"]
+                    else:
+                        phoneme = self.phoneme_map["singles"]["g"]["default"]
+                else:
+                    phoneme = self.phoneme_map["singles"][char]
+            else:
+                phoneme = char.upper() 
+            
+            phonemes.append(phoneme)
+            i += 1
+        
+        return phonemes
 
     def map_text(self, text):
         pass
 
 t = TextPreprocessing("Hello world 1234 $ % . I'am Ajay s vasan and I'am a DR.")
-# t.normalize()
-print(t.abrevations())
+print(t.numbersToText())
