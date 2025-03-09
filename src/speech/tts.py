c0666557708979f4196b83b315f822af199527a8
Ajaysvasan <ajay192006@gmail.com>
2025-03-09 20:22:53 +0530

    building phoneme map

--- src/speech/tts.py
+++ src/speech/tts.py
@@ -24,6 +24,22 @@ class TextPreprocessing:
                 preproceedText += string + " "
         self.text = preproceedText.strip()
 
+class PhonemeMapper:
+    def __init__(self):
+        self.phoneme_map = self.build_phoneme_map()
+
+    def build_phoneme_map(self):
+        pass
+
+    def apply_contextual_rules(self, word):
+        pass
+
+    def map_word_to_phonemes(self, word):
+        pass
+
+    def map_text(self, text):
+        pass
+
 t = TextPreprocessing("Hello world 1234 $ % . I'am Ajay s vasan and I'am a DR.")
 # t.normalize()
 print(t.abrevations())
