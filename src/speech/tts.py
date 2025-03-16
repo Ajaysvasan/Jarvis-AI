d7c7735bb08544d78e3b800516827e424966813a
Ajaysvasan <ajay192006@gmail.com>
2025-03-17 00:02:03 +0530

    changes again getting tired of this project fr

---
+++ src/speech/tts.py
@@ -0,0 +1,19 @@
+
+
+class TTSConfig:
+    pass
+
+class TextPreprocessor:
+    pass
+
+class FastSpeech2Synthesizer:
+    pass
+
+class HiFiGANVocoder:
+    pass
+
+class TTS:
+    pass
+
+    def synthesize(self, text: str, output_file: str = "output.wav"):
+        pass
